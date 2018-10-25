ff2858c54fe017bc7e966f75b918db011d1ecdea
kaiweifuyonuiyo <43640501+kaiweiyonuiyo@users.noreply.github.com>
2018-10-25 19:15:19 +0800

    Update 251傅楷崴.cpp

--- 251傅楷崴.cpp
+++ 251傅楷崴.cpp
@@ -7,9 +7,9 @@ int money;
 cout << "說!你口袋有多少錢:";
 cin >> money;
 
-int x = money / a, f = money - x * a; 
-cout <<"這麼少阿...最少有" << e << "個50元 ";
-int g=f/b,h=f-g*b;
+int x = money / a, y = money - x * a; 
+cout <<"這麼少阿...最少有" << x << "個50元 ";
+int g=y/b,h=y-g*b;
 cout <<"," << g << "個10元";
 int i=h/c,j=h-i*c;
 cout<<","<<i<<"個5元";
